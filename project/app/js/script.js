81a573ae22dfcf1aae0453d8379ab00761907fca
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-12 10:09:20 +0100

    create hamburger animation

--- project/app/js/script.js
+++ project/app/js/script.js
@@ -1,1 +1,15 @@
-console.log('hello, world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', () => { // close hamburger menu
+  if(header.classList.contains('open')) {
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else { // open hamburger menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+})
