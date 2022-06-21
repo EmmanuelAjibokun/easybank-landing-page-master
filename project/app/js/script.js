ed6dbe7a3d3e534c9d4a6a38aea3b189fe73db67
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-21 18:17:08 +0100

    finish header section
    Header is now fully responsive, with hamburger menu for mobile view

--- project/app/js/script.js
+++ project/app/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body')
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', () => { // close hamburger menu
   if(header.classList.contains('open')) {
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    body.classList.remove('noScroll')
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    })
   } else { // open hamburger menu
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    body.classList.add('noScroll')
+    fadeElems.forEach((element) => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
   }
 })
